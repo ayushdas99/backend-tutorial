55c4a29d8f76c84040a1574c7fb47fe074b63ee7
ayushdas99 <ayushd99@gmail.com>
2022-03-14 18:54:29 +0530

    express and mongoose

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 3000
+const connectDB = require('./config/db')
+const { errorHandler } = require('./middleware/errorMiddleware')
+
+connectDB()
+
+const app = express() 
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => {
+    console.log(`Working on ${port}`)
+})
